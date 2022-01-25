d48d8e475e26da4bf359b29eaa8cf7e7679070ec
Roger Consul <rogerconsul@yahoo.com.br>
2022-01-25 17:04:28 -0300

    requisito 3

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return
+  }
+  const bixos = species.map((bixo) => bixo.name === animal.name);
+  return bixos.residents.length;
+  console.log(bixos);
 }
 
 module.exports = countAnimals;
