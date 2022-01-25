e5b4e2f36425e9cea25b9a85f7a48a139405a70f
Roger Consul <rogerconsul@yahoo.com.br>
2022-01-25 19:07:45 -0300

    Resolvido parte do 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,11 +3,20 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   if (!animal) {
-    return
+    const objeto = {};
+    species.forEach((specie) => {
+      objeto[specie.name] = specie.residents.length;
+    });
+    return objeto;
   }
-  const bixos = species.map((bixo) => bixo.name === animal.name);
-  return bixos.residents.length;
-  console.log(bixos);
+  if (!animal.sex) { // mentoria do Lucas me ajudou a definir isto
+    const bixos = species.find((bixo) => bixo.name === animal.specie);
+    return bixos.residents.length;
+  }
+  const quantidade = species.find((serVivo) => serVivo.name === animal.specie).filter(
+    (cavalo) => cavalo.sex === animal.sex,
+  );
+  return quantidade.length;
 }
 
 module.exports = countAnimals;
