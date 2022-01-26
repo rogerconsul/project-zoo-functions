d2a139d379584857c2481c8bd4962cb9157b1ef9
Roger Consul <rogerconsul@yahoo.com.br>
2022-01-26 17:37:27 -0300

    Resolvido metade do 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const pessoas = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+
+  pessoas.child = entrants.filter((entrant) =>
+    entrant.age < 18).length; // ideia do length veio da sala 1, onde me lembraram que o filter retorna array.
+
+  pessoas.adult = entrants.filter((entrant) =>
+    entrant.age >= 18 && entrant.age < 50).length;
+
+  pessoas.senior = entrants.filter((entrant) =>
+    entrant.age >= 50).length;
+
+  return pessoas;
 }
 
 function calculateEntry(entrants) {
