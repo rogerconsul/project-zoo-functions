5f36e0edc59f902d5fb4333dbfb133d47c992544
Roger Consul <rogerconsul@yahoo.com.br>
2022-01-25 22:58:23 -0300

    resolvido exercicio 4. Acho que consegui entender

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const isManager = ((id) => employees.some((emplo) => (
+  emplo.managers.find((manager) => manager === id)
+))
+);
 
+// O erro eu consegui resolver sozinho.
+// A parte de retornar o array eu precisei da ajuda da sala de estudos. Acho que consegui entender :|
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const responsavelPor = employees.filter((func) => func.managers.find(
+    (gerente) => gerente === managerId,
+  ));
+  return responsavelPor.map((peao) => `${peao.firstName} ${peao.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
