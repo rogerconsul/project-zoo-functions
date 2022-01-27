f5aec2aa1d24257993ff01de6e5e68f95c8167df
Roger Consul <rogerconsul@yahoo.com.br>
2022-01-27 16:28:48 -0300

    Resolvido requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const capturaAnimalId = employees.find((funcionario) => funcionario.id === id).responsibleFor[0];
+
+  const capturaArray = species.find((objeto) => objeto.id === capturaAnimalId).residents.sort(
+    (animalA, animalB) => animalB.age - animalA.age,
+  )[0]; // Grande Gean ajudou a reduzir o codigo ao inserir o index diretamente aqui.
+
+  return Object.values(capturaArray);
 }
 
 module.exports = getOldestFromFirstSpecies;
