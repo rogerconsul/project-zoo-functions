52c1396602321736791e429006f71250ad59ff95
Roger Consul <rogerconsul@yahoo.com.br>
2022-01-27 18:53:10 -0300

    Feito uma parte da 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,27 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeesCoverage(input) {
+  if (!input) {
+    const objetoResultado = {};
+    const inputInvalido = employees.map((funcionario) => {
+      objetoResultado.id = `${funcionario.id}`;
+      objetoResultado.fullName = `${funcionario.firstName} ${funcionario.lastName}`;
+      objetoResultado.species = [`${Object.values(funcionario.responsibleFor)}`];
+      objetoResultado.locations = ['xablasu'];
+      return objetoResultado;
+    });
+    return inputInvalido;
+  }
+
+  if (input) {
+    const descobrePeao = employees.find((peao) => peao.id === input.id || peao
+      .firstName === input.name || peao.lastName === input.name);
+    return descobrePeao;
+  }
 }
+
+console.log(getEmployeesCoverage());
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 module.exports = getEmployeesCoverage;
