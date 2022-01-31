54f9706b3897cba99c5a65bde296e426e9141709
Roger Consul <rogerconsul@yahoo.com.br>
2022-01-31 12:29:24 -0300

    Resolvido um problema de Lint. Dividida a funçao em 3 funçoeszinhas

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,9 +1,9 @@
 const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage(input) {
+function inputInvalido(input) {
   if (!input) {
-    const inputInvalido = employees.map((funcionario) => {
+    const invalido = employees.map((funcionario) => {
       const objetoResultado = {
         id: `${funcionario.id}`,
         fullName: `${funcionario.firstName} ${funcionario.lastName}`,
@@ -13,16 +13,34 @@ function getEmployeesCoverage(input) {
       };
       return objetoResultado;
     });
-    return inputInvalido;
-  }
-  if (input) {
-    const descobrePeao = employees.find((peao) => peao.id === input.id || peao
-      .firstName === input.name || peao.lastName === input.name);
-    return descobrePeao;
+    return invalido;
   }
 }
 
-console.log(getEmployeesCoverage());
+function inputValido(input) {
+  if (input) {
+    const descobrePeao = employees.find((peao) => peao.id === input.id || peao
+      .firstName === input.name || peao.lastName === input.name);
+    return {
+      id: `${descobrePeao.id}`,
+      fullName: `${descobrePeao.firstName} ${descobrePeao.lastName}`,
+      species: Object.values(descobrePeao.responsibleFor)
+        .map((id) => species.find((nome) => nome.id === id).name),
+      locations: ['xablasu'],
+    };
+  }
+}
+
+function getEmployeesCoverage(input) {
+  if (!input) {
+    return inputInvalido(input);
+  }
+  if (input) {
+    return inputValido(input);
+  }
+}
+
+// console.log(getEmployeesCoverage());
 // console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 module.exports = getEmployeesCoverage;
